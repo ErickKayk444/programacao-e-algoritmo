8a3f105472a048a1aaa10e8bf83989dbef790e11
Kayk Lima <matheuskayk811@gmail.com>
2026-09-25 12:14:06 -0300

    Create exercicio5.py

---
+++ 3-bimestre/python-lista/exercicio5.py
@@ -0,0 +1,10 @@
+n = int(input('Digite um número inteiro positivo de 1 a 10, que iremos mostrar a sua tabuada: '))
+contador = 1;
+
+while n < 1 or n > 10:
+    print("Valor inválido")
+    n = int(input("Digite outro número: "))
+
+while contador <= 10:
+    print(f'{n} x {contador} = {n * contador}')
+    contador += 1
